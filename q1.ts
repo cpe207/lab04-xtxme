3f911d8b9c1cb018f5a443a54e2e46a3e170e85b
xtxme <TT.icy013@gmail.com>
2024-07-02 14:54:22 +0700

    แก้รอบ1

--- q1.ts
+++ q1.ts
@@ -1,17 +1,17 @@
 //660610762 นางสาวธนพร ตั้งผดุงสุข
 function typeChecker(a, b) {
-  if (typeof a == typeof b) {
-      if (typeof a == 'string') {
-          return 'I LOVE YOU';
+    if(typeof a === typeof b){
+      if(typeof a === 'string') {
+        return "I LOVE YOU";
       }
-      else {
-          return a + b;
-      }
+      else  if(typeof a === 'number') {
+        return (a+b);
+    }
   }
-  else {
-      return 'NOT MATCHED';
+    else{
+      return "NOT MATCHED";
+    }
   }
-}
 
 //Test cases
 const a1 = "Jack";
